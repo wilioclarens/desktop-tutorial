ab80b914c73f44a0bd647bb58ab4d66f905aedbf
wilio_clarens <167869692+wilioclarens@users.noreply.github.com>
2024-05-14 23:58:18 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+!DOCTYPE html>
+<html>
+    <head>
+        <title> conexao </title>
+        <meta Charset="utf-8">
+</head>
+<body>
+
+        <form method="POST" action="" align="center">
+            <input type="text" name="name">
+            <br/>
+            <input type="password" name="senha">
+            <br/>
+            <input type="submit" name="enviar">
+        </form>
