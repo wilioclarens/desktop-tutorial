b3f2ffdd5528a5355bc262488c0b5530964174ad
wilio_clarens <167869692+wilioclarens@users.noreply.github.com>
2024-05-16 11:18:46 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,4 +1,27 @@
 <?php
+session_start();
+$bdd=new PDO('mysql:host=localhost;dbname=clinica;charset=utf8;','root','Wilio@18061994');
+if(isset($_POST['enviar'])){
+    if(!empty($_POST['name']) AND !empty($_POST['senha'])){
+        $name= htmlspecialchars($_POST['senha']);//Proibir o usuário de carregar código HTML ou JavaScript
+        $senha=sha1($_POST['senha']);//Método SHA1 para criptografar a senha
+        $inserUser= $bdd->prepare('INSERT INTO users(name,senha) VALUES(?,?)');
+        $inserUser->execute(array($name,$senha));
+
+        $recupUser=$bdd->prepare('SELECT * FROM users WHERE name= ? AND senha = ?');
+        $recupUser->execute(array($name,$senha));
+        
+        if ($recupUser->rowcount() > 0){
+            $_SESSION['name']= $name;
+            $_SESSION['senha']= $seha;
+            $_SESSION['id']=$recupUser->fetch()['id'];
+
+        }else{ echo "Seu nome ou senha está incorreto, por favor, verifique novamente, obrigado!"}
+
+    } else{
+        echo "Por favor, preencha todos os campos";
+    }
+}
 !DOCTYPE html>
 <html>
     <head>
@@ -8,9 +31,9 @@
 <body>
 
         <form method="POST" action="" align="center">
-            <input type="text" name="name">
+            <input type="text" name="name" autocomplete="off">
             <br/>
-            <input type="password" name="senha">
+            <input type="password" name="senha" autocompleto="off">
             <br/>
             <input type="submit" name="enviar">
         </form>
