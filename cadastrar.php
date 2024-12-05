064a43cdce2791e9e25939fdd142531cd41e49a3
wilio_clarens <167869692+wilioclarens@users.noreply.github.com>
2024-12-05 11:42:46 -0300

    Update cadastro.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,50 +1,31 @@
-<?php
-session_start();
-$bdd = new PDO('mysql:host=localhost;dbname=Clinica;charset=utf8;','root','Wilio@18061994');
-if(isset($_POST['enviar'])){
-    if(!empty($_POST['name']) AND !empty($_POST['senha']) AND !empty($_POST['email'])){
-        $name= htmlspecialchars($_POST['name']);
-        $cpf= htmlspecialchars($_POST['cpf']);
-        $email= htmlspecialchars($_POST['email']);
-        $contact= htmlspecialchars($_POST['contact']);
-        $senha=sha1($_POST['senha']);
-        $inserUser = $bdd->prepare('INSERT INTO users(name,cpf,email,contact,senha)VALUES(?,?,?,?,?)');
-        $inserUser->execute(array($name, $cpf, $email, $contact, $senha));
-       
-        $recupUser = $bdd->prepare('SELECT * FROM users WHERE name = ? AND senha=?');
-        $recupUser->execute(array($name,$senha));
-        if($recupUser->rowCount() >0){
-            
-        $_SESSION['name']= $name;
-        $_SESSION['senha']= $senha;
-        $_SESSION['id']= $recupUser->fetch()['id'];}
-
-        echo $_SESSION['id'];
-
-    } else{ echo"por favor verifique novamente";}
-    }
-    ?>
 <!DOCTYPE html>
+
 <html>
-    ,<head>
-        <title> se cadastrar</title>
-        <meta charset="utf-8">
+    <head>
+    <meta charset="UTF-8">
+        <title> Formulário de Cadastro </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="estiloLogin.css"/>
     </head>
     <body>
-        <form method="POST" action="" align="center">
-            <input type="text" name="name" autocomplete="off">
-            <br/>
-            <input type="text" name="cpf"  autocoplete="off">
-            <br/>
-            <input type="text" name="email" >
-            <br/>
-            <input type="text" name="contact">
-            <br/>
-            <input type="password" name="senha" autocomplete="off" >
-            <br/>
-            <input type="submit" name="enviar">
-
+        <div class="login-form p-5 my-5 bg-info text-white">
+        <form action='cadastroacao.php' method='post'>
+            <strong style="margin-right: 145px;"> Nome </strong></br>
+            <input type="text" name="nome"  placeholder="Digite seu nome"/><br/>
+            <strong style="margin-right: 160px;"> CPF </strong></br>
+            <input type="text" name="cpf"  placeholder="Digite seu CPF"/><br/>
+            <strong style="margin-right: 145px;"> Email </strong></br>
+            <input type="text" name="email" placeholder="Digite seu email"/><br/>
+            <strong style="margin-right: 125px;"> Telefone </strong></br>
+            <input type="text" name="telefone" placeholder="Digite seu telefone"/><br/>
+            <strong style="margin-right: 140px;"> Senha </strong></br>
+            <input type="text" name="senha" placeholder="Digite sua senha"/><br/>
+            <input type="submit" value="enviar" class="btn btn-light mt-3"/>
         </form>
-
+</div>
     </body>
-    </html>
+</html>
